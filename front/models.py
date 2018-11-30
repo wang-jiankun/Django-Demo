cceb0b4394417e5a5c027f6b290b5a0bc7a2b9d2
jkwang <924837562@qq.com>
2018-11-30 17:07:17 +0800

    2018-11-30

--- front/models.py
+++ front/models.py
@@ -6,15 +6,24 @@ from django.db import models
 
 
 class Author(models.Model):
+    """
+    作者重要信息表
+    """
     name = models.CharField(max_length=100)
 
 
 class AuthorInfo(models.Model):
+    """
+    作者详细信息表，一对一（一个作者行对应唯一的详细信息行）
+    """
     country = models.CharField(max_length=100)
     author = models.OneToOneField("Author", on_delete=models.CASCADE, related_name='info')
 
 
 class Article(models.Model):
+    """
+    文章信息表，一对多（一个作者可以有多篇文章）
+    """
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey("Author", on_delete=models.CASCADE, null=True, related_name='articles')
@@ -24,5 +33,8 @@ class Article(models.Model):
 
 
 class Tag(models.Model):
+    """
+    文章标签信息表，多对多（一篇文章有多个标签，一个标签可以对应多篇文章）
+    """
     name = models.CharField(max_length=100)
     articles = models.ManyToManyField("Article", related_name='tags')
--- front/views.py
+++ front/views.py
@@ -5,23 +5,36 @@ from front.models import Article, Tag, Author, AuthorInfo
 
 
 def index(request):
+    """
+    首页视图函数，显示所有文章信息
+    :param request:
+    :return:
+    """
     articles = Article.objects.all()
     return render(request, 'index.html', context={"articles": articles})
 
 
 def add_article(request):
+    """
+    添加文章视图函数，发布一篇文章
+    :param request:
+    :return:
+    """
     if request.method == 'GET':
         return render(request, 'add_article.html')
     else:
         title = request.POST.get("title")
         content = request.POST.get("content")
         author_name = request.POST.get("author")
+        # 从数据库中查找该作者是否已经存在，如果不存在则添加到数据库中
         author = Author.objects.filter(name=author_name).first()
         if not author:
             author = Author(name=author_name)
             author.save()
+        # 文章信息存储到数据库中
         article = Article(title=title, content=content, author=author)
         article.save()
+        # 文章标签存储到数据库中
         tag = request.POST.get("tag")
         tag = Tag(name=tag)
         tag.save()
@@ -30,16 +43,35 @@ def add_article(request):
 
 
 def article_detail(request, article_id):
+    """
+    文章详情页视图函数
+    :param request:
+    :param article_id:
+    :return:
+    """
+    # 根据文章 id 查找文章
     article = Article.objects.get(id=article_id)
     return render(request, 'article_detail.html', context={"article": article})
 
 
 def author_articles(request, author_id):
+    """
+    作者信息详情页视图函数
+    :param request:
+    :param author_id:
+    :return:
+    """
+    # 根据作者 id 查找作者
     author = Author.objects.get(id=author_id)
     return render(request, 'author_articles.html', context={"author": author})
 
 
 def delete_article(request):
+    """
+    删除文章视图函数
+    :param request:
+    :return:
+    """
     if request.method == 'POST':
         article_id = int(request.POST.get('article_id'))
         article = Article.objects.get(id=article_id)
